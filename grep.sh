226c43b6421cc77d675c686f6d0f85a45c7bc45e
Badr.Ch <bch6882@gmail.com>
2025-01-26 22:07:23 +0100

    Add files via upload

--- grep.sh
+++ grep.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 if [ $1 == "-h" ]; then
-	echo "usage : ./grep.sh (execut the script)"
+	echo "usage : ./grep.sh {file} {option} {word}"
  	echo " option :"
 	echo "-n : show the number of the line . "
  	echo "-w : search for word . "
-fi
-
-echo " what is the file path : "
-read  file
-
-echo " what is the word that you looking for :"
-read  word
-
-echo " what is the option that you want : "
-read  option
-
-if [ $file ] && [ $word ] && [ $option ] && [ ! $1 = "-h" ] ;then
-	op=$( cat $file |grep $option --color=always $word )
-	echo " $op "
+elif [ $1 ] && [ $2 ] && [ $3 ] ;then
+        op=$( cat $1 |grep $2 --color=always $3 )
+        echo " $op "
 else
-	echo " your input are wgron !!! "
+        echo " your input are wgron !! "
 fi
